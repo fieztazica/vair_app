e7c5386d13ea3bc1f692984c687f9ff0b80bfe2f
Dat Hoang <fiezt@outlook.com>
2023-12-15 11:25:32 +0700

    home tab improved

--- lib/controllers/home_tab_controller.dart
+++ lib/controllers/home_tab_controller.dart
@@ -1,26 +1,33 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
 import 'package:vair_app/helpers/api_endpoints.dart';
+import 'package:vair_app/models/Category.dart';
 import 'package:vair_app/models/Product.dart';
 
 class HomeTabController extends GetxController with StateMixin<List<Product>> {
   Rxn<List<Product>> products = Rxn();
+  RxList<Widget> categories = <Widget>[].obs;
+  Rx<String> selectedCategory = Rx("All");
 
   @override
   void onInit() {
     super.onInit();
     change(null, status: RxStatus.empty());
+    getCategories();
     getData();
   }
 
-  void getData() async {
+  void getData({String category = ""}) async {
     try {
       change(null, status: RxStatus.loading());
 
+      var query = category.isEmpty ? "" : "?category=${category}";
+
       var res = await get(Uri.parse(
-          "${ApiEndPoints.expressBaseApiURL}/${ApiEndPoints.productEndPoints.base()}"));
+          "${ApiEndPoints.expressBaseApiURL}/${ApiEndPoints.productEndPoints.base()}$query"));
 
       // print("API Response: ${res.statusCode} - ${res.body}");
 
@@ -48,8 +55,54 @@ class HomeTabController extends GetxController with StateMixin<List<Product>> {
     }
   }
 
+  void getCategories() async {
+    try {
+      var res =
+          await get(Uri.parse("${ApiEndPoints.expressBaseApiURL}/categories"));
+
+      // print("API Response: ${res.statusCode} - ${res.body}");
+
+      if (res.statusCode == 200 && res.body != null) {
+        var responseBody = json.decode(res.body);
+
+        if (responseBody['data'] is List) {
+          List<Category> categories = List<Category>.from(
+            (responseBody['data'] as List)
+                .map((dynamic item) => Category.fromJson(item)),
+          );
+          this.categories.value = renderCategoryButtons(categories);
+        } else {
+          // Handle the case when the "data" field is not a list
+          throw "Invalid response format: ${res.body}";
+        }
+      } else {
+        print("API Response: ${res?.statusCode} - ${res?.body}");
+        throw "API Response Failed: ${res.body}";
+      }
+    } catch (e) {
+      print("Error in getData: $e");
+    }
+  }
+
+  List<Widget> renderCategoryButtons(List<Category> categories) {
+    categories = [Category(name: "All"), ...categories];
+    return categories
+        .map(
+          (e) => Padding(
+              padding: EdgeInsets.symmetric(horizontal: 2),
+              child: OutlinedButton(
+                  onPressed: () {
+                    selectedCategory.value = e.name!;
+                    getData(category: e.name!);
+                  },
+                  child: Text("${e.name}"))),
+        )
+        .toList();
+  }
+
   Future<void> refreshData() async {
     getData();
+    getCategories();
   }
 
   void onClose() {
--- lib/screens/tabs/main_screen/home_tab.dart
+++ lib/screens/tabs/main_screen/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vair_app/controllers/home_tab_controller.dart';
@@ -18,7 +19,10 @@ class HomeTab extends StatelessWidget {
     return RefreshIndicator(
         onRefresh: _refresh,
         child: _homeTabController.obx(
-          (state) => _buildProductLists(state!.cast<Product>()),
+          (state) => _buildProductLists(
+              state!.cast<Product>(),
+              _homeTabController.categories,
+              _homeTabController.selectedCategory.value),
           onLoading: const Center(
             child: CircularProgressIndicator(),
           ),
@@ -31,13 +35,16 @@ class HomeTab extends StatelessWidget {
         ));
   }
 
-  Widget _buildProductLists(List<Product> products) {
-    List<Product> shuffledProducts1 = List.from(products)..shuffle();
-    List<Product> shuffledProducts2 = List.from(products)..shuffle();
-    List<Product> shuffledProducts3 = List.from(products)..shuffle();
-    List<Product> shuffledProducts4 = List.from(products)..shuffle();
-    return ListView(
-      children: [
+  Widget _buildProductLists(List<Product> products,
+      List<Widget> categoriesButtons, String selectedCategory) {
+    List<Widget> widgets = [];
+    if (selectedCategory == "All") {
+      List<Product> shuffledProducts1 = List.from(products)..shuffle();
+      List<Product> shuffledProducts2 = List.from(products)..shuffle();
+      List<Product> shuffledProducts3 = List.from(products)..shuffle();
+      List<Product> shuffledProducts4 = List.from(products)..shuffle();
+
+      widgets = [
         HorizontalProductList(
           items: shuffledProducts1,
           title: "Featured & Recommended",
@@ -53,7 +60,51 @@ class HomeTab extends StatelessWidget {
         HorizontalProductList(
           items: shuffledProducts4,
           title: 'Indie games',
+        )
+      ];
+    } else {
+      print(products.length);
+      if (products.isNotEmpty) {
+        widgets = products
+            .map((e) => ListTile(
+                  title: Text(e.name!),
+                  subtitle: e.price! > 0
+                      ? Text(
+                          'VND ${e.price!.toStringAsFixed(2)}',
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      : const Text('Free'),
+                  leading: ClipOval(
+                    child: CachedNetworkImage(
+                      imageUrl: e.logo!.uri.toString(),
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ))
+            .toList();
+      } else {
+        widgets = [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text("Nothing found..."),
+          )
+        ];
+      }
+    }
+
+    return ListView(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          height: 40.0,
+          child: new ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[...categoriesButtons],
+          ),
         ),
+        ...widgets
       ],
     );
   }
