0c092d9c3a4bfb12a0ca6c013ba3fa717314a34a
Truong <huynhnhattruonglt@gmail.com>
2023-12-15 13:39:24 +0700

    minor fix

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:vair_app/helpers/api_endpoints.dart';
-import 'search_result_screen.dart';
+import 'package:vair_app/models/Product.dart';
+import 'package:vair_app/routes/app_pages.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
+
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _searchController = TextEditingController();
+  List<dynamic> searchResults = [];
+
+  Future<void> _searchAndNavigate() async {
+    String query = _searchController.text;
+    if (query.isNotEmpty) {
+      List<dynamic> results = await _searchProducts(query);
+      setState(() {
+        searchResults = results;
+      });
+    }
+  }
 
   Future<List<dynamic>> _searchProducts(String query) async {
     try {
@@ -22,7 +36,6 @@ class _SearchScreenState extends State<SearchScreen> {
 
       if (response.statusCode == 200) {
         print(json.decode(response.body)['data']);
-
         return json.decode(response.body)['data'];
       } else {
         // Handle error
@@ -36,12 +49,8 @@ class _SearchScreenState extends State<SearchScreen> {
     }
   }
 
-  void _searchAndNavigate() async {
-    String query = _searchController.text;
-    if (query.isNotEmpty) {
-      List<dynamic> searchResults = await _searchProducts(query);
-      Get.to(() => SearchResultScreen(searchResults));
-    }
+  void _navigateToProductDetail(int productId) {
+    Get.toNamed(Routes.DETAIL, parameters: {"id": productId.toString()});
   }
 
   @override
@@ -65,8 +74,43 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
             ),
           ),
+          Expanded(
+            child: _buildSearchResults(),
+          ),
         ],
       ),
     );
+  }
+
+  Widget _buildSearchResults() {
+    if (searchResults.isEmpty) {
+      return Center(
+        child: Text('No results found'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: searchResults.length,
+        itemBuilder: (context, index) {
+          Product product = Product.fromJson(searchResults[index]);
+          return GestureDetector(
+            onTap: () {
+              int productId = product.id!;
+              _navigateToProductDetail(productId);
+            },
+            child: ListTile(
+              title: Text(product.name!),
+              leading: ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: product.logo!.uri.toString(),
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
   }
 }
